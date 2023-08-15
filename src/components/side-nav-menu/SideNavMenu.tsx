02d367c1bcb60f19979c15d7b18d7c43746840a1
v.shtyl <vlad751392@gmail.com>
2023-08-15 12:54:09 +0300

    updated SideNavMenu component

--- src/components/side-nav-menu/SideNavMenu.tsx
+++ src/components/side-nav-menu/SideNavMenu.tsx
@@ -3,8 +3,9 @@ import { FC, ReactNode, useState } from "react";
 import React from "react";
 
 import { Button } from "../button";
+import "./styles.css";
 
-import { ReactComponent as ArrowIcon } from "@svg/menu-arrow.svg";
+// import ArrowIcon from "./assets/png/test.png";
 
 interface Props {
   children: ReactNode;
@@ -17,29 +18,30 @@ export const SideNavMenu: FC<Props> = ({ children }) => {
 
   return (
     <nav
-      className={cn(
-        "grid grid-cols-0 min-h-0 relative grid-rows-nav gap-2 bg-blue-400 p-2 ease-in-out transition-all duration-500",
-        {
-          "grid-cols-auto": isOpened,
-        }
-      )}
+      className={cn("grid min-h-0 relative gap-2 p-2 nav-animation", {
+        "nav-opened": isOpened,
+      })}
     >
       <a href="#" className="mb-10 select-none">
         Logo
       </a>
-      <Button
-        onClick={onMenuClick}
-        className="border-none absolute -right-4 top-11 p-2 !rounded-full hover:bg-blue-600 bg-amber-600"
-      >
-        <ArrowIcon
-          className={cn(
-            "stroke-white transition-all ease-in-out duration-500 delay-300 w-4 h-4",
-            {
-              "rotate-180": isOpened,
-            }
-          )}
-        />
-      </Button>
+      <div className="button-wrapper">
+        <Button
+          onClick={onMenuClick}
+          className="border-none absolute button top-11 p-2 hover:bg-blue-600 bg-amber-600 rounded-full"
+        >
+          <p
+            className={cn(
+              "stroke-white transition-all text-white ease-in-out duration-500 delay-300",
+              {
+                "arrow-animation": isOpened,
+              }
+            )}
+          >
+            ❯
+          </p>
+        </Button>
+      </div>
       {children}
     </nav>
   );
