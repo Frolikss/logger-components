2fc3c2d504ab9441808c8d15d352c287375b511a
Frolikss <darkness1198@gmail.com>
2023-08-29 11:09:36 +0300

    table fix

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import React, { FC, Fragment, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import moment from 'moment';
 
 export interface Config<T> {
@@ -46,122 +46,96 @@ export const Table: FC<Props<any>> = ({ items, config, pageNumber }) => {
 
   if (!Array.isArray(items)) return <div>No data available</div>;
   return (
-    <>
-      <table className="text-center w-full">
-        <thead className="border-b-2 text-xs">
-          <tr>
+      <>
+        <table className="w-full">
+          <thead className="bg-dim-blue text-xs">
+          <tr className="[&>*:nth-last-child(2)]:rounded-r-md [&>*:nth-last-child(2)]:text-center">
             {config.map((cfg, index) => (
-              <th key={index} className="py-2 font-normal">
-                {cfg.title}
-              </th>
+                <th
+                    key={index}
+                    className="py-4 first:rounded-l-md last:hidden px-1.5 text-left font-medium">
+                  {cfg.title}
+                </th>
             ))}
           </tr>
-        </thead>
-        <tbody className="relative">
+          </thead>
+          <tbody className="relative text-sm">
           {items.map((item) => (
-            <Fragment key={item.id}>
-              <tr className="border-b-1">
-                {config.map(
-                  (cfg, cfgIndex) =>
-                    cfg.type !== 'drop' && (
-                      <td
-                        key={cfgIndex}
-                        className={cn(
-                          'py-2 font-normal',
-                          { 'flex justify-center': cfg.value === 'components' },
-                          cfg.classNames && cfg.classNames(item)
-                        )}>
-                        {cfg.value === 'components' ? (
-                          <>
-                            {cfg.components?.(item, pageNumber ?? 0) ?? null}
-                            <svg
-                              className={cn('cursor-pointer hover:fill-gray-300', {
-                                'rotate-180': selectedId === item.id
-                              })}
-                              onClick={onDropDownClick(item.id)}
-                              xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" version="1.1" width="30px" height="30px" viewBox="0 0 48 48"
-                            >
-                              <path  d="
-  M 43.93 24.00
-  A 19.93 19.93 0.0 0 1 24.00 43.93
-  A 19.93 19.93 0.0 0 1 4.07 24.00
-  A 19.93 19.93 0.0 0 1 24.00 4.07
-  A 19.93 19.93 0.0 0 1 43.93 24.00
-  Z"
-                              />
-                              <path fill="#ffffff" d="
-  M 24.16 26.74
-  L 31.76 19.41
-  A 0.40 0.40 0.0 0 1 32.31 19.41
-  L 34.82 21.76
-  A 0.40 0.40 0.0 0 1 34.83 22.33
-  L 24.27 32.84
-  A 0.40 0.40 0.0 0 1 23.71 32.84
-  L 13.28 22.41
-  A 0.40 0.40 0.0 0 1 13.28 21.85
-  L 15.72 19.41
-  A 0.40 0.40 0.0 0 1 16.28 19.41
-  L 23.60 26.73
-  A 0.40 0.40 0.0 0 0 24.16 26.74
-  Z"
-                              />
-                            </svg>
-
-                          </>
-                        ) : cfg.value === 'status' ? (
-                          <>{cfg.components?.(item, pageNumber ?? 0) ?? null}</>
-                        ) : cfg.date ? (
-                          getFormattedDate(item[cfg.value])
-                        ) : (
-                          item[cfg.value]
-                        )}
-                      </td>
-                    )
-                )}
-              </tr>
-              {isDrop && (
-                <tr>
-                  <td colSpan={config.length} className="p-0">
-                    <div
-                      className={cn(
-                        'transition-all [&>*]:overflow-hidden [&>*:last-child]:order-first grid grid-rows-0 grid-cols-auto',
-                        {
-                          'grid-rows-auto': selectedId === item.id
-                        }
-                      )}>
-                      <div className="flex gap-2 justify-end flex-row-reverse">
-                        {dropDownItems.map((dropItem, index) => (
-                          <div
-                            key={item.id + index}
-                            className="px-4 flex flex-row-reverse items-center">
-                            {!dropItem.img ? (
-                              <div dangerouslySetInnerHTML={{ __html: item[dropItem.value] }} />
-                            ) : (
-                              <div className="[&>*]:h-28 [&>*]:w-44 [&>*]:flex [&>*]:justify-center [&>*]:items-center">
-                                {item[dropItem.value] ? (
-                                  <img
-                                    className="object-contain"
-                                    src={`${process.env.REACT_APP_IMAGES_URL}${
-                                      item[dropItem.value]
-                                    }`}
-                                    alt="banner"
-                                  />
+              <React.Fragment key={item.id}>
+                <tr
+                    className={cn('border-b-1 cursor-pointer transition-all rounded-md', {
+                      'hover:bg-dim-blue': isDrop
+                    })}
+                    onClick={onDropDownClick(item.id)}>
+                  {config.map(
+                      (cfg, cfgIndex) =>
+                          cfg.type !== 'drop' && (
+                              <td
+                                  key={cfgIndex}
+                                  className={cn(
+                                      'py-4 px-1.5 font-normal first:rounded-l-md last:rounded-r-md',
+                                      { 'flex justify-center': cfg.value === 'components' },
+                                      cfg.classNames && cfg.classNames(item)
+                                  )}>
+                                {cfg.value === 'components' ? (
+                                    <>{cfg.components?.(item, pageNumber ? pageNumber : 0) ?? null}</>
+                                ) : cfg.value === 'status' || cfg.value === 'type' ? (
+                                    <>
+                                      {cfg.components?.(item, pageNumber !== undefined ? pageNumber : 0) ??
+                                          null}
+                                    </>
+                                ) : cfg.date ? (
+                                    getFormattedDate(item[cfg.value])
                                 ) : (
-                                  <p>No Image</p>
+                                    item[cfg.value]
                                 )}
-                              </div>
-                            )}
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </td>
+                              </td>
+                          )
+                  )}
                 </tr>
-              )}
-            </Fragment>
+                {isDrop && (
+                    <tr>
+                      <td colSpan={config.length} className="p-0">
+                        <div
+                            className={cn(
+                                'transition-all [&>*]:overflow-hidden [&>*:last-child]:order-first grid grid-rows-0 grid-cols-auto',
+                                {
+                                  'grid-rows-auto': selectedId === item.id
+                                }
+                            )}>
+                          <div className="flex gap-2 justify-end flex-row-reverse">
+                            {dropDownItems.map((dropItem, index) => (
+                                <div
+                                    key={item.id + index}
+                                    className="px-4 flex flex-row-reverse items-center">
+                                  {!dropItem.img ? (
+                                      <div dangerouslySetInnerHTML={{ __html: item[dropItem.value] }} />
+                                  ) : (
+                                      <div className="[&>*]:h-28 [&>*]:w-44 [&>*]:flex [&>*]:justify-center [&>*]:items-center">
+                                        {item[dropItem.value] ? (
+                                            <img
+                                                className="object-contain"
+                                                src={`${process.env.REACT_APP_IMAGES_URL}${
+                                                    item[dropItem.value]
+                                                }`}
+                                                alt="banner"
+                                            />
+                                        ) : (
+                                            <p>No Image</p>
+                                        )}
+                                      </div>
+                                  )}
+                                </div>
+                            ))}
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                )}
+              </React.Fragment>
           ))}
-        </tbody>
-      </table>
-    </>
+          </tbody>
+        </table>
+      </>
   );
 };
