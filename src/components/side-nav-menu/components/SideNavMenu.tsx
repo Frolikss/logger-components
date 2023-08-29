f8e0b3baeb60e07f3393657a52fe1c6f9db20b58
Frolikss <darkness1198@gmail.com>
2023-08-29 11:12:55 +0300

    table fix

--- src/components/side-nav-menu/components/SideNavMenu.tsx
+++ src/components/side-nav-menu/components/SideNavMenu.tsx
@@ -1,5 +1,4 @@
-import cn from 'classnames';
-import React, { FC, SVGProps } from 'react';
+import React, {FC, ReactNode, SVGProps} from 'react';
 
 export interface MenuItem {
     path: string;
@@ -8,30 +7,17 @@ export interface MenuItem {
 }
 
 interface Props {
-  config: MenuItem[];
-  pathname: string
+  children: ReactNode;
 }
 
-export const SideNavMenu: FC<Props> = ({ config, pathname }) => {
+export const SideNavMenu: FC<Props> = ({ children }) => {
   return (
     <nav
       className="flex flex-col gap-2 bg-blue-400 p-2 transition-all">
       <a href="/" className="mb-4">
         Logo
       </a>
-      {config.map(({ path, icon: Icon, text }) => (
-        <a
-          key={path}
-          href={path}
-          className={cn(
-            'flex gap-2 self-start transition-all p-1.5 rounded-full',
-            { 'bg-amber-600': path === pathname }
-          )}>
-          <Icon
-            className="fill-white transition-all w-6 shrink-0"
-          />
-          <span className="text-white">{text}</span>
-        </a>
+        {children}
       ))}
     </nav>
   );
